281d38a6bb6f22322a7a08d96f218e8fa2f6013d
olegdayo <ol-sidorenkov@mail.ru>
2025-04-04 17:39:20 +0300

    clients

--- internal/clients/auth/wrapper.go
+++ internal/clients/auth/wrapper.go
@@ -1,1 +1,21 @@
 package auth
+
+import (
+	"context"
+	"net/http"
+)
+
+type Wrapper struct {
+	url string
+}
+
+func New(url string) Wrapper {
+	return Wrapper{
+		url: url,
+	}
+}
+
+func (w Wrapper) Do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	req = req.WithContext(ctx)
+	return http.DefaultClient.Do(req)
+}
