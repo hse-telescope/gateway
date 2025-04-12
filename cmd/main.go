ea80fd4ff87c76aa6dfec9eeefcb6da990d5fb23
olegdayo <ol-sidorenkov@mail.ru>
2025-04-12 21:10:36 +0300

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"os"
+
+	"github.com/hse-telescope/gateway/internal/app"
+	"github.com/hse-telescope/gateway/internal/config"
+)
+
 func main() {
+	configPath := os.Args[1]
+	conf, err := config.Parse(configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	app := app.New(conf)
+	panic(app.Start())
 }
